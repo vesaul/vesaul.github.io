dc1cbd97eb0c9a0b0a475fc3b03c0e31083a928e
Vesa Ulvi <vesa.ulvi@gmail.com>
2016-07-12 00:03:52 +0300

    - more typo corrections - js reformatting

--- assets/js/form_submission_handler.js
+++ assets/js/form_submission_handler.js
@@ -4,49 +4,51 @@ function validEmail(email) { // see:
 }
 
 // get all data in form and return object
+
 function getFormData() {
   var elements = document.getElementById("gform").elements; // all form elements
   var fields = Object.keys(elements).map(function(k) {
-    if(elements[k].name !== undefined) {
+    if (elements[k].name !== undefined) {
       return elements[k].name;
     }
   }).filter(function(item, pos, self) {
     return self.indexOf(item) == pos && item;
   });
   var data = {};
-  fields.forEach(function(k){
+  fields.forEach(function(k) {
     data[k] = elements[k].value;
   });
   console.log(data);
   return data;
 }
 
-function handleFormSubmit(event) {  // handles form submit withtout any jquery
-  event.preventDefault();           // we are submitting via xhr below
-  var data = getFormData();         // get the values submitted in the form
-  if( !validEmail(data.email) ) {   // if email is not valid show error
+function handleFormSubmit(event) { // handles form submit withtout any jquery
+  event.preventDefault(); // we are submitting via xhr below
+  var data = getFormData(); // get the values submitted in the form
+  if (!validEmail(data.email)) { // if email is not valid show error
     document.getElementById('email-invalid').style.display = 'block';
     return false;
   } else {
-    var url = event.target.action;  //
+    var url = event.target.action; //
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     // xhr.withCredentials = true;
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
-        console.log( xhr.status, xhr.statusText )
-        console.log(xhr.responseText);
-        document.getElementById('gform').style.display = 'none'; // hide form
-        document.getElementById('thankyou_message').style.display = 'block';
-        return;
+      console.log(xhr.status, xhr.statusText)
+      console.log(xhr.responseText);
+      document.getElementById('gform').style.display = 'none'; // hide form
+      document.getElementById('thankyou_message').style.display = 'block';
+      return;
     };
     // url encode form data for sending as post data
     var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
     }).join('&')
     xhr.send(encoded);
   }
 }
+
 function loaded() {
   console.log('contact form submission handler loaded successfully');
   // bind to the submit event of our form
